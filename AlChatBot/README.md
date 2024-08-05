0edd758581afd3991789b53dab7165185b356d2a
Mehmet <cakamehmet280@gmail.com>
2024-08-05 18:06:30 +0300

    AlChatBot
    I created this project using gemma2:2b from ollama. I used Vite and React technologies for the frontend. I used node.js and hono technologies for the backend.

---
+++ AlChatBot/README.md
@@ -0,0 +1,2 @@
+# AiChatBot
+ I created this project using gemma2:2b from ollama. I used Vite and React technologies for the frontend. I used node.js and hono technologies for the backend.
---
+++ AlChatBot/react-ollama-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+//import './App.css'
+import ChatUI from "./components/chatUI.tsx";
+
+function App() {
+
+  return (
+    <>
+      <div className="app">
+          <ChatUI />
+      </div>
+    </>
+  )
+}
+
+export default App
